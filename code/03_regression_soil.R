8fc95ae9cdc26d158af63d7dfda4c68ec1b4a740
marijokerud <mari.jokerud@gmail.com>
2024-02-26 09:45:43 +0100

    changes to soil chemistry script

--- code/03_regression_soil.R
+++ code/03_regression_soil.R
@@ -1,43 +1,38 @@
 library(lme4)
-#mod <- datLM %>% 
-#group_by(heavymetal) %>% 
-#do(fit = lm(dose ~  aar, data = .))
+library(xlsx)
 
-#names(mod)
-#res.mod<-tidy(mod, fit)
-#tibble(res.ratio)<-tidy(result.ratio, fit)
+outputSoil = soilLM %>% 
+  # replace group_by() with nest_by() 
+  # to convert your model data to a vector of lists
+  nest_by(element) %>%
+  # change do() to mutate(), then add list() before your model
+  # make sure to change data = .  to data = data
+  mutate(fitSoil = list(lm(measurement ~ Year, data = data))) %>%
+  reframe(tidy(fitSoil))
 
-summary(lm(pH~year,data=soil))
-summary(lm(N_Kjeldahl~year,data=soil))
-summary(lm(C_N~year,data=soil))
+outputSoil
 
-#PLOT
-soilPLOT<- soil %>%
-  select(-P) %>% #only values for 2023
-  gather("type", "dose", pH, N_Kjeldahl, C_N, K, Mg, Na_AL) %>% 
-  group_by(year, type) %>% 
-  summarise(avg=mean(dose), SD=sd(dose)) 
+resSoil = soilLM %>% 
+  nest_by(element) %>%
+  mutate(fitSoil = list(lm(measurement ~ Year, data = data))) %>%
+  reframe(glance(fitSoil))
+
+resSoil
+
+write.xlsx(resSoil, file = "output/outputSoil2.xlsx", col.names = TRUE, row.names = TRUE)
 
 
-ggplot(soilPLOT, aes(x=year, y=avg)) + 
-  geom_line(lwd=1, position = position_dodge(width = 0.4)) +
-  geom_point(cex=3, position = position_dodge(width = 0.4)) +
-  geom_linerange(aes(ymin=avg-(SD*2/sqrt(6)), ymax=avg+(SD*2/sqrt(6))), position = position_dodge(width = 0.4)) +
-  facet_grid(type~., scales="free_y") +
-  ylab("") +
-  xlab("År") +
-  scale_x_continuous(breaks=c(1995, 1998, 2003, 2008, 2013, 2018, 2023)) +
-  #scale_colour_manual(values=cbbPalette) +
-  theme_bw() +
-  theme(legend.position="none") +
-  theme(strip.text.y = element_text(size = 12),
-        axis.title.x = element_text(size=12,hjust=0.5),
-        axis.title.y = element_text(size=12,vjust=1),
-        axis.text.x = element_text(size=12,color='black'),
-        axis.text.y = element_text(size=12,color='black'),
-        legend.title = element_text( color="black", size=18),
-        legend.text = element_text( color="black", size=12)) +
-  theme(panel.grid.minor.x=element_blank(),
-        panel.grid.major.x=element_blank(),
-        panel.grid.major.y=element_blank(),
-        panel.grid.minor.y=element_blank())
+
+
+###################
+summary(lm(Kalium~Year,data=soil))
+summary(lm(Kalsium~Year,data=soil))
+summary(lm(Magnesium~Year,data=soil))
+summary(lm(Natrium~Year,data=soil))
+summary(lm(LOI~Year,data=soil))
+summary(lm(TOC~Year,data=soil))
+summary(lm(Nitrogen~Year,data=soil))
+summary(lm(C_N~Year,data=soil))
+
+
+
--- code/04_figure_soil.R
+++ code/04_figure_soil.R
@@ -1,17 +1,25 @@
 library(ggplot2)
 
-ggplot(datSUM, aes(x=Aar, y=avg, group=Provetype, colour=Provetype)) + 
-  geom_line(lwd=1, position = position_dodge(width = 0.4)) +
-  geom_point(cex=3, position = position_dodge(width = 0.4)) +
-  geom_linerange(aes(ymin=avg-(SD*2/sqrt(6)), ymax=avg+(SD*2/sqrt(6))), position = position_dodge(width = 0.4)) +
-  facet_grid(heavymetal~., scales="free_y") +
-  ylab("mg/kg") +
+elementPLOT <- soilLM %>% 
+  filter(element %in% c("Kalium", "Kalsium", "Magnesium", "Natrium"))
+
+nutrientPLOT <- soilLM %>% 
+  filter(element %in% c("pH", "Karbon", "Nitrogen", "CN", "LOI"))
+
+
+cbbPalette <- c("#018C95", "#E57201", "#93328E", "#7A9A01") #lilla = #93328E, turkis = #018C95, grønn = #7A9A01, oransje = #E57201
+
+mineralplott <-
+ggplot(elementPLOT, aes(x=Year, y=measurement, group=element, colour=element)) +
+  facet_wrap(element ~ ., scales = "free") +
+  stat_smooth(method="lm", se=TRUE) +
+  ylab("mg/100g") +
   xlab("År") +
-  scale_x_continuous(breaks=c(2006, 2008, 2013, 2018, 2021)) +
+  #scale_x_continuous(breaks=c(2006, 2008, 2013, 2018, 2021)) +
   scale_colour_manual(values=cbbPalette) +
   theme_bw() +
   theme(legend.position="none") +
-  theme(strip.text.y = element_text(size = 12),
+  theme(strip.text.x = element_text(size = 16),
         axis.title.x = element_text(size=12,hjust=0.5),
         axis.title.y = element_text(size=12,vjust=1),
         axis.text.x = element_text(size=12,color='black'),
@@ -20,6 +28,41 @@ ggplot(datSUM, aes(x=Aar, y=avg, group=Provetype, colour=Provetype)) +
         legend.text = element_text( color="black", size=12)) +
   theme(panel.grid.minor.x=element_blank(),
         panel.grid.major.x=element_blank(),
-        panel.grid.minor.y=element_blank())
+        panel.grid.major.y=element_blank(),
+        panel.grid.minor.y=element_blank()) #Hide all the vertical gridlines
 
+png(filename = "output/mineralplot.png", width = 850, height = 650, units = "px", res = 96)   # or tiff("plot.tiff")
+plot(mineralplott)
+dev.off()
+
+cbbPalette <- c("#018C95", "#E57201", "#93328E", "#7A9A01", "#980043")
+
+
+nutrientplott <-
+ggplot(nutrientPLOT, aes(x=Year, y=measurement, group=element, colour=element)) +
+  facet_wrap(element ~ ., scales = "free", ncol = 2) +
+  stat_smooth(method="lm", se=TRUE) +
+  ylab("mg/100g") +
+  xlab("År") +
+  #scale_x_continuous(labels=c("0" = "1995", "3" = "1998", "8" = "2003", "13" = "2008", "18" = "2013", "23" = "2018", "28" = "2023")) +
+  scale_colour_manual(values=cbbPalette) +
+  theme_bw() +
+  theme(legend.position="none") +
+  theme(strip.text.x = element_text(size = 16),
+        axis.title.x = element_text(size=12,hjust=0.5),
+        axis.title.y = element_text(size=12,vjust=1),
+        axis.text.x = element_text(size=12,color='black'),
+        axis.text.y = element_text(size=12,color='black'),
+        legend.title = element_text( color="black", size=18),
+        legend.text = element_text( color="black", size=12)) +
+  theme(panel.grid.minor.x=element_blank(),
+        panel.grid.major.x=element_blank(),
+        panel.grid.major.y=element_blank(),
+        panel.grid.minor.y=element_blank()) #Hide all the vertical gridlines
+
+png(filename = "output/nutrientplot.png", width = 850, height = 950, units = "px", res = 96)   # or tiff("plot.tiff")
+plot(nutrientplott)
+dev.off()
 #save as 800x650
+
+
